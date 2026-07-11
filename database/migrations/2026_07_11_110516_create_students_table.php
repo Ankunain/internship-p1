1cac128113a6e4efe7f09f07efeb650b2e7cdc87
Ankush Nain <ankunain8571@gmail.com>
2026-07-11 16:56:24 +0530

    a

---
+++ database/migrations/2026_07_11_110516_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('name')->nullable()->default('student');
+            $table->string('roll_no')->nullable();
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('email')->nullable();
+            $table->string('class')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
